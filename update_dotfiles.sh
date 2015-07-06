9f296d9851493810e1b939311001cad8e51963a0
Benjamin Calderon <benj.calderon@gmail.com>
2015-07-06 11:50:56 -0400

    added more stuff

---
+++ update_dotfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# copy bin folder
+cp -f ~/bin/* ~/dotfiles/bin
+
+# copy configs
+cp -f ~/.vimrc ~/dotfiles
+cp -f ~/.config/bspwm/bspwmrc ~/dotfiles/.config/bspwm/bspwmrc
+cp -f ~/.config/sxhkd/sxhkdrc ~/dotfiles/.config/sxhkd/sxhkdrc
+
+
